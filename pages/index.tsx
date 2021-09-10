2eac34f5c0aafa7732d0d0056265820dfc508851
Aleksandr Kornetov <aakornetov@yandex.ru>
2021-09-10 14:20:08 +0300

    Created Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next';
-import { Htag } from '../components';
+import { Htag, Button } from '../components';
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <>
       <Htag tag="h1">Header</Htag>
+      <Button theme="primary" arrow="right">
+        Button
+      </Button>
+      <Button theme="ghost" arrow="down">
+        Button
+      </Button>
     </>
   );
 };
