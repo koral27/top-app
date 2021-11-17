7a3bd36c2ddcba256dfadfb3203dff946bccbdc3
Корнетов Александр Алексеевич <AAKornetov@sberbank.ru>
2021-11-17 14:22:51 +0300

    Add courses.[alias].tsx template with data

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,7 @@ export default withLayout(Home);
 export const getStaticProps: GetStaticProps = async () => {
   const firstCategory = 0;
   const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory });
+
   return {
     props: {
       menu,
