c81a0362d77ab60d5fa9b370c7386fd8b1e36489
Корнетов Александр Алексеевич <AAKornetov@sberbank.ru>
2021-11-17 12:34:13 +0300

    Add getStaticProps to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 import { useState } from 'react';
+import axios from 'axios'
 import { Htag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-const Home: NextPage = (): JSX.Element => {
+const Home: NextPage<HomeProps> = ({ menu }): JSX.Element => {
   const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag="h1">Header</Htag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu && menu.map(item => <li key={item._id.secondCategory} >{item._id.secondCategory}</li>)}
+      </ul>
     </>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
